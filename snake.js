467528175b1671301531e1a2ada986d83d3c0162
badboy1496 <36558440+badboy1496@users.noreply.github.com>
2018-03-22 11:16:56 +0530

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,163 @@
+//Snake
+
+function init(){
+    // console.log("Init");
+    canvas = document.getElementById('mycanvas');
+    pen = canvas.getContext('2d');
+    W = canvas.width;
+    H = canvas.height;
+    gameover= false;
+    // box = {
+    //     x : 10 , y : 20 , w : 20 , h : 20, speed : 5,
+    // }
+    // Getting food 
+    food = getRandomFood();
+    score = 5;
+    //snake object
+    snake = {
+        init_length : 5 ,
+        color : "yellow" ,
+        cells : [],
+        direction : "right" ,
+        creatSnake : function(){
+            for(var i = this.init_length-1;i>=0;i--)
+            {
+                this.cells.push({ x : i, y : 0});
+            }
+        },
+        drawSnake : function(){
+            for(var i=0;i<this.cells.length;i++){
+                pen.fillStyle = this.color;
+                
+                pen.strokeStyle = "black"; 
+
+                pen.lineWidth = 0; 
+                
+                pen.strokeRect(this.cells[i].x*10,this.cells[i].y*10,10,10);
+                pen.fillRect(this.cells[i].x*10,this.cells[i].y*10,10,10);
+            }
+        },
+        updateSnake : function(){
+            var headX = this.cells[0].x;
+            var headY = this.cells[0].y;
+            //Assumig Snake is moving right
+            //Insertion at head
+            // nextHeadX = headX+1;
+            
+            // this.cells.unshift({x : nextHeadX , y : headY})
+
+            //eating food logic
+            if(headX == food.x && headY == food.y)
+            {
+                food = getRandomFood();
+                score++;
+            }
+            else
+            {
+                this.cells.pop();
+            }
+            //taking keys input for moving in direction
+            if(this.direction == "right"){
+                nextX= headX + 1;
+                nextY = headY;
+            }
+            else if(this.direction == "left")
+            {
+                nextX = headX-1;
+                nextY = headY ; 
+            }
+            else if(this.direction== "down")
+            {
+                nextX = headX;
+                nextY = headY+1;
+            }
+            else
+            {
+                nextX = headX;
+                nextY = headY-1;
+            }
+            //Insert the new  cell at head/front
+            this.cells.unshift({x : nextX , y : nextY});
+
+            //find our the last coodinates (boundaries)
+            var last_x = Math.round(W/10);
+            var last_y = Math.round(H/10);
+
+            if(this.cells[0].y<0 ||  this.cells[0].x< 0 || this.cells[0].x>last_x || this.cells[0].y>last_y)
+            {
+                alert("GameOver");
+                gameover = true;
+            }
+        }, 
+
+    };
+    snake.creatSnake();
+    // Add Event Listeners to our game
+    // Listen for keyboard events.
+    function KeyPressed(e){
+        if(e.key == "ArrowRight")
+        {
+            snake.direction = "right";
+        }
+        else if(e.key == "ArrowLeft")
+        {
+            snake.direction="left";
+        }
+        else if(e.key == "ArrowDown")
+        {
+            snake.direction= "down";
+        }
+        else 
+        {
+            snake.direction = "up";
+        }
+    }
+    document.addEventListener('keydown',KeyPressed);
+}
+
+function draw(){
+    pen.clearRect(0,0,W,H);
+    snake.drawSnake(); 
+
+    //drawing food
+    pen.fillStyle = food.color;
+    pen.fillRect(food.x*10,food.y*10,10,10);   
+
+    //displaying score 
+    pen.fillStyle = "white";
+    pen.front = "14px Roboto";
+    pen.fillText("Score : " + score,10,10);     
+}
+
+function update(){
+    snake.updateSnake();
+}
+
+function gameLoop(){
+    draw();
+    update();
+    if(gameover==true){
+        clearInterval(f);
+    }
+}
+
+function getRandomFood(){
+    var foodX = Math.round(Math.random()*(W-10)/10);
+    var foodY = Math.round(Math.random()*(H-10)/10);
+
+    foodColors = ["red","green","aqua","coral","orchid"];
+    
+    var i= Math.round(Math.random()*foodColors.length);
+
+    var food = {
+        x : foodX,
+        y : foodY,
+        color : foodColors[i],
+    }
+    return food;
+}
+init();
+//gameLoop();
+
+// Call Game loop after t time(time in ms)
+var f= setInterval(gameLoop,100);
